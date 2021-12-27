f685da9e39b9c6338178db616c44685b8d48b7c8
InWooChoi <zeus0711@naver.com>
2021-12-27 14:01:04 +0900

    1227 오전

--- src/test/java/org/conan/test/DataSourceTest.java
+++ src/test/java/org/conan/test/DataSourceTest.java
@@ -3,6 +3,9 @@ package org.conan.test;
 import static org.junit.Assert.fail;
 import java.sql.Connection;
 import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +18,27 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DataSourceTest {
+	
+	/*
+	 * @Setter(onMethod_= {@Autowired}) private DataSource ds;
+	 * 
+	 * @Test public void testConnection() { try(Connection con =
+	 * ds.getConnection()){ log.info(con); }catch(Exception e) {
+	 * fail(e.getMessage()); } }
+	 */
+	
 	@Setter(onMethod_= {@Autowired})
-	private DataSource ds;
+	private SqlSessionFactory sqlSessionFactory;
+	
 	@Test
-	public void testConnection() {
-		try(Connection con = ds.getConnection()){
-			log.info(con);
-		}catch(Exception e) {
-			fail(e.getMessage());
-		}
+	public void testMyBatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();){
+					log.info(session);log.info(con);
+				}catch(Exception e) {
+					fail(e.getMessage());
+				}
+				
 	}
 
 }
